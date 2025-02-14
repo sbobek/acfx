fb8a79c8e6a05b212e562a0b4628dd34dc04fe35
Szymon Bobek <szymon.bobek@gmail.com>
2025-02-14 17:25:20 +0100

    Structure added, documentation added.

--- src/acfx/model.py
+++ src/acfx/model.py
@@ -9,6 +9,14 @@ class ACFX(BaseEstimator, TransformerMixin):
         pass
 
     def fit(self, X=None, y=None):
+        """
+        Fits explainer to the sampled data and blackbox model provided in the constructor
+
+        :param X:
+        :type X: pd.DataFrame
+        :param y:
+        :return:
+        """
         return self
 
     def predict(self, X):
