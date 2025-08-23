61b1cd1acc0287aa4f1d93c422e30e6a720d5186
Piotr Kubacki <74492669+she3r@users.noreply.github.com>
2025-08-23 16:42:34 +0200

    poprawki fit classifier

--- src/app/pages/2_classifier_selection.py
+++ src/app/pages/2_classifier_selection.py
@@ -16,8 +16,15 @@ classifier_list = [
     ]
 
 def init_classifier():
+    if st.session_state.X is None:
+        raise ValueError("Data for classifier fit is not available (st.session_state.X)")
+    if st.session_state.y is None:
+        raise ValueError("Data for classifier fit is not available (st.session_state.y)")
     classifier = None
     params = st.session_state.classifier_params
+    selected_feature_labels = [item["Column Name"] for _, item in st.session_state.feature_types.iterrows() if item["is_on"]]
+    selected_feature_types = [item["Type"] for _, item in st.session_state.feature_types.iterrows() if item["is_on"]]
+    selected_X = st.session_state.X[selected_feature_labels]
     if params is not None:
         if st.session_state.classifier_name == classifier_list[0]:
             classifier = LogisticRegression(**params)
@@ -26,14 +33,11 @@ def init_classifier():
         elif st.session_state.classifier_name == classifier_list[2]:
             classifier = KNeighborsClassifier(**params)
         elif st.session_state.classifier_name == classifier_list[3]:
-            classifier = ExplainableBoostingClassifier(**params)
+            classifier = ExplainableBoostingClassifier(feature_types= selected_feature_types, **params)
         else:
             raise IndexError("classifier_name out of range")
-        if st.session_state.X is None:
-            raise ValueError("Data for classifier fit is not available (st.session_state.X)")
-        if st.session_state.y is None:
-            raise ValueError("Data for classifier fit is not available (st.session_state.y)")
-        classifier = classifier.fit(st.session_state.X,st.session_state.y)
+
+        classifier = classifier.fit(selected_X,st.session_state.y)
 
     st.session_state.classifier_instance = classifier
 
